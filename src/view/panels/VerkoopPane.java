c18178f4d30b398e0e19b69ef9a8a6bcea210e3b
Hans <hans.pauwels@student.ucll.be>
2019-12-03 15:37:15 +0100

    Added tableview

--- src/view/panels/VerkoopPane.java
+++ src/view/panels/VerkoopPane.java
@@ -1,9 +1,6 @@
 package view.panels;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 public class VerkoopPane extends GridPane {
@@ -12,10 +9,16 @@ public class VerkoopPane extends GridPane {
     private static Button submitBtn = new Button("Bevestig");
     private Label errorLabel = new Label();
     private TableView tableView = new TableView();
+
+    TableColumn omschrCol = new TableColumn("Omschrijving");
+    TableColumn prijsCol = new TableColumn("prijs");
+    TableColumn aantalCol = new TableColumn("Aantal");
+
     public VerkoopPane() {
         this.add(enterLabel,0,0);
         this.add(codeTextField,0,1);
         this.add(errorLabel, 0, 2);
+        tableView.getColumns().addAll(omschrCol, prijsCol, aantalCol);
         this.add(tableView, 0,3);
     }
 
@@ -25,5 +28,9 @@ public class VerkoopPane extends GridPane {
 
     public Label getErrorLabel() {
         return errorLabel;
+    }
+
+    public TableView getTableView() {
+        return tableView;
     }
 }
