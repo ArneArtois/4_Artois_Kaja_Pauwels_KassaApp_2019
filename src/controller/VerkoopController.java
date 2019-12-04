961c84e09fe99de45f28a3f7da7ba92e1a8d5cae
Hans <hans.pauwels@student.ucll.be>
2019-12-04 16:23:14 +0100

    Added Verkoop MVC

---
+++ src/controller/VerkoopController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import database.ArtikelDBContext;
+import database.strategy.ArtikelDBStrategy;
+import javafx.scene.control.TableView;
+import model.Artikel;
+import model.Verkoop;
+import model.observer.Observer;
+import view.panels.VerkoopPane;
+
+import java.util.List;
+
+public class VerkoopController implements Observer {
+    private Verkoop verkoop;
+    private VerkoopPane verkoopPane;
+    private double totalePrijs = 0;
+    private ArtikelDBContext context;
+
+    public VerkoopController(Verkoop verkoopModel, ArtikelDBContext context) {
+        this.verkoop = verkoopModel;
+        verkoopModel.registerObserver(this);
+        this.context = context;
+    }
+
+    public void setVerkoopPane(VerkoopPane verkoopPane) {
+        if(verkoopPane == null) {
+            throw new IllegalArgumentException("VerkoopPane mag niet leeg zijn");
+        }
+
+        this.verkoopPane = verkoopPane;
+    }
+
+    public double getTotalePrijs() {
+        return this.totalePrijs;
+    }
+
+    public void codeEnter(int code) {
+        Artikel a = context.get(code);
+        verkoop.addArtikel(context.get(code));
+    }
+
+    @Override
+    public void update(Artikel a, List<Artikel> artikelen) {
+        this.totalePrijs += a.getVerkoopprijs();
+        verkoopPane.updateDisplay(this.totalePrijs, artikelen);
+    }
+}
