df906fe7864fccf771c6b49d5177d1a51ad3bc17
Hans <hans.pauwels@student.ucll.be>
2019-12-15 16:07:45 +0100

    Formatting van kassaticket verbeterd

--- src/model/decorator/KassaTicketImplementation.java
+++ src/model/decorator/KassaTicketImplementation.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class KassaTicketImplementation implements KassaTicket {
 
     public String print(List<Artikel> artikelen, double betaaldePrijs) {
-        String result = "Omschrijving\t\tAantal\tPrijs\n****************************************\n";
+        String result = "Omschrijving\t\tAantal\t\tPrijs\n****************************************\n";
         for(Artikel a: artikelen) {
-            result += a.getOmschrijving()+"\t\t\t\t\t"+a.getAantalPerKeer()+"\t\t\t"+a.getVerkoopprijs()+"\n";
+            result += a.getOmschrijving()+"\t\t\t\t"+a.getAantalPerKeer()+"\t\t\t"+a.getVerkoopprijs()+"\n";
         }
         result += "****************************************\nBetaald (inclusief korting): " + betaaldePrijs;
 
